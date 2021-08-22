070e5b178db4eac426b5501391d59ff87a4c93e1
romana <romana@mislbd.com>
2021-08-22 15:08:46 +0600

    new project create in SwiftUI

---
+++ SSLCommerzIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSLCommerzIntegration
+//
+//  Created by Romana on 22/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SSLCommerzIntegration/SSLCommerzIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSLCommerzIntegrationApp.swift
+//  SSLCommerzIntegration
+//
+//  Created by Romana on 22/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SSLCommerzIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
